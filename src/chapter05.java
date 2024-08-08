61a58f85ce68b0b207de4ee5e48b2bdad7a3751c
SenaKanetaka <177509930+Jungkoon@users.noreply.github.com>
2024-08-08 19:56:26 +0900

    確認問題

---
+++ src/chapter05.java
@@ -0,0 +1,15 @@
+
+public class chapter05 {
+    
+	public static void main(String[] args) {
+		String str1;
+		str1 = "金高世那";
+		System.out.println(str1);
+		
+		str1 = "BTS";
+		System.out.println(str1);
+		
+		final int num1 = 5;
+		System.out.println(num1);
+	}
+}
